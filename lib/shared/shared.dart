81dcefe863b5f40749f91b7a9abd38a5dde5ecf8
Ferdy Adriantama <ferdypbg@gmail.com>
2023-02-18 13:44:29 +0700

    refactor: Change font, asset folder and shared theme

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,6 +1,5 @@
 import 'package:cinematix/bloc/blocs.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 part 'shared_value.dart';
 part 'theme.dart';
--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -2,21 +2,24 @@ part of 'shared.dart';
 
 const double defaultMargin = 24;
 
+Color whitebackgroundColor = const Color.fromRGBO(255, 255, 255, 1.0);
+Color whitebackgroundColor2 = const Color.fromRGBO(246, 247, 249, 1.0);
+
 Color mainColor = const Color(0xFF503E9D);
 Color accentColor1 = const Color(0xFF2C1F63);
 Color accentColor2 = const Color(0xFFFBD460);
 Color accentColor3 = const Color(0xFFADADAD);
 
-TextStyle blackTextFont = GoogleFonts.raleway()
+TextStyle blackTextFont = const TextStyle(fontFamily: 'Raleway')
     .copyWith(color: Colors.black, fontWeight: FontWeight.w500);
-TextStyle whiteTextFont = GoogleFonts.raleway()
+TextStyle whiteTextFont = const TextStyle(fontFamily: 'Raleway')
     .copyWith(color: Colors.white, fontWeight: FontWeight.w500);
-TextStyle purpleTextFont = GoogleFonts.raleway()
+TextStyle purpleTextFont = const TextStyle(fontFamily: 'Raleway')
     .copyWith(color: mainColor, fontWeight: FontWeight.w500);
-TextStyle greyTextFont = GoogleFonts.raleway()
+TextStyle greyTextFont = const TextStyle(fontFamily: 'Raleway')
     .copyWith(color: accentColor3, fontWeight: FontWeight.w500);
 
 TextStyle whiteNumberFont =
-    GoogleFonts.openSans().copyWith(color: Colors.white);
+    const TextStyle(fontFamily: 'OpenSans').copyWith(color: Colors.white);
 TextStyle yellowNumberFont =
-    GoogleFonts.openSans().copyWith(color: accentColor2);
+    const TextStyle(fontFamily: 'OpenSans').copyWith(color: accentColor2);
