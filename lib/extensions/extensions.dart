323effc931a55e198c7836db4b4e80d691854198
Ferdy Adriantama <ferdypbg@gmail.com>
2023-02-19 23:53:32 +0700

    feat(extension): Create method getMaterialColor for Color Extension
    Color extension getMaterialColor is a method to convert Color to

--- lib/extensions/extensions.dart
+++ lib/extensions/extensions.dart
@@ -3,5 +3,7 @@ import 'dart:convert';
 import 'package:cinematix/models/models.dart';
 import 'package:cinematix/services/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 part 'firebase_user_extension.dart';
+part 'color_extension.dart';
